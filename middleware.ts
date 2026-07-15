80ca0dca1e7b2ea08236a96862ce312fc1e8830f
laxman3452 <contactlaxman99@gmail.com>
2026-07-15 08:24:45 +0545

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,8 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 
-import type { NextRequest } from "next/server";
-
 const { auth } = NextAuth(authConfig);
-
-export default async function middleware(req: NextRequest) {
-  return await auth(req);
-}
+export default auth;
 
 export const config = {
   // Skip static files, _next internals, and API auth routes
